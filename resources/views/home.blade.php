9431f661553c912c7ba2fef375bf8d6e0c124703
joel-irineu <irineu.joel01@gmail.com>
2022-08-24 12:07:50 -0300

    alterações iniciais

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <h1>Seja Bem-Vindo!</h1>
                 </div>
             </div>
         </div>
